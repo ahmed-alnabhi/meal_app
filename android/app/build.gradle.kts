5c1b88440dcc103cf21bebc565e4b3c4226709e3
Ahmed  Al-Nabehi <ahmed2004.1436@gmail.com>
2025-08-13 17:55:21 +0300

    Update app package name and dependencies; enhance app icons and manifest files for iOS and Android. configure flutter_launcher_icons for custom app icons.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.meal_app"
+    namespace = "com.mealapp.name"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.meal_app"
+        applicationId = "com.mealapp.name"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/mealapp/name/MainActivity.kt
+++ android/app/src/main/kotlin/com/mealapp/name/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.meal_app
+package com.mealapp.name
 
 import io.flutter.embedding.android.FlutterActivity
 
